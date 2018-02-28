7cc20256af797c76a19ae0060d7ebfea9a9d6b9c
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-02-28 09:23:37 -0500

    back to clean Angular project

--- GetGewd/src/app/app.module.ts
+++ GetGewd/src/app/app.module.ts
@@ -1,38 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-/** import AngularFire **/
-import { AngularFireModule } from 'angularfire2';
-
-/** export firebaseConfig **/
-export const firebaseConfig = {
-	apiKey: '',
-	authDomain: '',
-	databaseURL: '',
-	storageBucket: '',
-	messagingSenderId: ''
-};
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { EmailComponent } from './email/email.component';
-import { SignupComponent } from './signup/signup.component';
-import { MembersComponent } from './members/members.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    EmailComponent,
-    SignupComponent,
-    MembersComponent
+    AppComponent
+
   ],
   imports: [
-    BrowserModule,
-    // Firebase  modules
-    FormsModule,
-    HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
