51d85d6d27248c4ec53539395070d53f2a1ffa72
Sleepy122 <Calder.Jones3155@gmail.com>
2018-03-05 23:53:56 -0500

    updates

--- GetGewd/src/app/app-routing.module.ts
+++ GetGewd/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { JoinPageComponent } from './components/join-page/join-page.component';
 import { ProgressPageComponent } from './components/progress-page/progress-page.component';
 import { CreateGroupPageComponent } from './components/create-group-page/create-group-page.component';
-import {AddFlashcardPageComponent} from "./components/add-flashcard-page/add-flashcard-page.component"
+import {AddFlashcardPageComponent} from "./components/add-flashcard-page/add-flashcard-page.component";
+import { FlashcardPageComponent } from './components/flashcard-page/flashcard-page.component';
 
 const routes: Routes =[
 	{ path: '', redirectTo: '/login', pathMatch: 'full'},
@@ -16,6 +17,7 @@ const routes: Routes =[
   {path:'createGroup',component:CreateGroupPageComponent},
   {path:'progress',component:ProgressPageComponent},
   {path:'addFlashcards', component:AddFlashcardPageComponent},
+  {path: 'flashcards', component: FlashcardPageComponent},
 ];
 @NgModule({
   imports: [
---
+++ GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flashcard-wheel',
+  templateUrl: './flashcard-wheel.component.html',
+  styleUrls: ['./flashcard-wheel.component.css']
+})
+export class FlashcardWheelComponent implements OnInit {
+
+  constructor() { }
+  
+
+  ngOnInit() {
+  }
+
+}
