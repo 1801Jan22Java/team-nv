788b77879488e81c692c419ba65edd11799bf917
Sleepy122 <Calder.Jones3155@gmail.com>
2018-03-06 19:55:07 -0500

    small update

--- GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
+++ GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
@@ -6,22 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flashcard-wheel.component.css']
 })
 export class FlashcardWheelComponent implements OnInit {
+  private things:any;
 
   constructor() {
     
     
    }
 
-   reviewCards(){
-      let things=[{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerp",answer:"bloop",hint:"blap"}]
+   Cards(){
+      this.things =[{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerp",answer:"bloop",hint:"blap"}];
 
-      var question = document.createTextNode(things[0].question);
-      var answer = document.createTextNode(things[0].answer);
-      document.getElementById("Container").appendChild(question);
+      console.log(this.things[0].question);
+      console.log(this.things[0].answer);
+      var question = document.createTextNode(this.things[0].question);
+      var answer = document.createTextNode(this.things[0].answer);
+      //var holder = document.createElement("p");
+      //holder.appendChild(question);
+      document.getElementById("container").appendChild(question);
+   }
+   nextCard(){
+
+   }
+
+   previousCard (){
+
+   }
+
+   showCard(){
+     
    }
 
   ngOnInit() {
-
+    this.Cards();
   
   }
 }
