90594baa6a1380891f566106ff990237c14bee38
Sleepy122 <Calder.Jones3155@gmail.com>
2018-03-06 21:14:19 -0500

    FlashCards

--- GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
+++ GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
@@ -7,33 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlashcardWheelComponent implements OnInit {
   private things:any;
+  private position:number = 0;
 
-  constructor() {
-    
-    
+  constructor() {    
    }
 
    Cards(){
-      this.things =[{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerp",answer:"bloop",hint:"blap"}];
+      this.things =[{question:"blerp",answer:"bloop",hint:"blap"},{question:"blerpy",answer:"bloop",hint:"blap"},{question:"blerpest",answer:"bloop",hint:"blap"}];
 
-      console.log(this.things[0].question);
-      console.log(this.things[0].answer);
       var question = document.createTextNode(this.things[0].question);
-      var answer = document.createTextNode(this.things[0].answer);
       //var holder = document.createElement("p");
       //holder.appendChild(question);
       document.getElementById("container").appendChild(question);
+      console.log(this.position);
    }
    nextCard(){
+     console.log("things is "+this.things.length)
+     if(this.position == this.things.length-1){
+       this.position = 0
+       console.log("got here 0")
+     }else{
+     this.position += 1;
+     console.log("got here 1")
+     }
 
+     console.log(this.position);
+     //var holder = document.createElement("p");
+     //holder.appendChild(question);
+     document.getElementById("container").innerHTML =this.things[this.position].question;
    }
 
    previousCard (){
+    if(this.position = 0){
+      this.position = this.things.length;
+    }
+    this.position -= 1;
 
+    console.log(this.position);
+    var question = document.createTextNode(this.things[this.position].question);
+    //var holder = document.createElement("p");
+    //holder.appendChild(question);
+    document.getElementById("container").innerHTML =this.things[this.position].question;
    }
 
    showCard(){
-     
+   
+    //var holder = document.createElement("p");
+    //holder.appendChild(question);
+    document.getElementById("container").innerHTML =this.things[this.position].answer;
+
    }
 
   ngOnInit() {
