d1f6cd02e53c3903e1ffa4a5e924ce71c256c388
pharms2011 <prestonharms5@gmail.com>
2018-03-07 17:07:03 -0500

    fixed duplicate flashcards for groups

--- Project2/src/main/java/com/revature/main/Driver.java
+++ Project2/src/main/java/com/revature/main/Driver.java
@@ -1,19 +1,5 @@
 package com.revature.main;
 
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import com.revature.beans.Flashcard;
-import com.revature.beans.Group;
-import com.revature.beans.PendingFlashcard;
-import com.revature.beans.Tag;
-import com.revature.beans.Users;
-
-import com.revature.util.HibernateUtil;
-
-import DaoTests.DataBaseDriver;
-
 public class Driver {
 
 }
