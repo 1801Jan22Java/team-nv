02f1a1dd2fa7a6bdc3d6f1efc6b2618c40f5c2ba
Sleepy122 <Calder.Jones3155@gmail.com>
2018-03-06 21:41:11 -0500

    updates

--- GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
+++ GetGewd/src/app/components/pieces/flashcard-wheel/flashcard-wheel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/auth.service';
+import{HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-flashcard-wheel',
@@ -9,7 +11,7 @@ export class FlashcardWheelComponent implements OnInit {
   private things:any;
   private position:number = 0;
 
-  constructor() {    
+  constructor(public auth: AuthService,private httpClient:HttpClient) {    
    }
 
    Cards(){
@@ -57,9 +59,16 @@ export class FlashcardWheelComponent implements OnInit {
     document.getElementById("container").innerHTML =this.things[this.position].answer;
 
    }
+   showHint(){
+   
+    //var holder = document.createElement("p");
+    //holder.appendChild(question);
+    document.getElementById("container").innerHTML =this.things[this.position].hint;
+
+   }
 
   ngOnInit() {
-    this.Cards();
+
   
   }
 }
