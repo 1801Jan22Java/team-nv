be23403399f404e99f10b72e5068664a3c1a91d5
Sleepy122 <Calder.Jones3155@gmail.com>
2018-03-07 12:59:46 -0500

    updates

--- GetGewd/src/app/components/pieces/nav/nav.component.ts
+++ GetGewd/src/app/components/pieces/nav/nav.component.ts
@@ -10,7 +10,7 @@ export class NavComponent implements OnInit {
 
   constructor(public auth: AuthService, private route:Router ) { }
 
-  traveHome(){
+  travelHome(){
     this.route.navigate(["/homepage"]);
   }
 
